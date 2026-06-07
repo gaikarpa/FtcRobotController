e5e592b1281580085dbd0262d92e8d8e8b4bde54
Gavriil Karpathios <ga_karpa@hotmail.com>
2026-06-07 15:19:42 +0300

    Create GKRevTest.java
    Hello world

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GKRevTest.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class GKRevTest extends OpMode {
+    @Override
+    public void init() {
+        telemetry.addData("Hello", "World" );
+        telemetry.addData("Gavriil", "Karpathios" );
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
